8be927086929a6c8c7fcf1454720f270832dcf06
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-02-17 18:49:53 -0500

    Add exception checks for attenuation function parameters

--- src/dispersion4b/shortrange/attenuation.py
+++ src/dispersion4b/shortrange/attenuation.py
@@ -1,13 +1,33 @@
-import numpy as np
+import math
+from dataclasses import dataclass
 
 
-def silvera_goldman_attenuation(r: float, b0: float, r_cutoff: float) -> float:
+@dataclass(frozen=True)
+class SilveraGoldmanAttenuation:
     """
     The short-long attenuation function matching the form given in the
     Silvera-Goldman potential.
     """
-    if r > r_cutoff:
-        return 1.0
-    else:
-        exponent = ((r_cutoff / r) - 1.0) ** 2
-        return np.exp(-b0 * exponent)
+
+    r_cutoff: float
+    expon_coeff: float
+
+    def __post_init__(self) -> None:
+        if self.r_cutoff <= 0.0:
+            raise ValueError(
+                "The cutoff distance for the attenuation function must be positive.\n"
+                f"Entered: {self.r_cutoff: .12f}"
+            )
+
+        if self.expon_coeff <= 0.0:
+            raise ValueError(
+                "The coefficient in the exponent must be positive.\n"
+                f"Entered: {self.expon_coeff: .12f}"
+            )
+
+    def __call__(self, r: float) -> float:
+        if r >= self.r_cutoff:
+            return 1.0
+        else:
+            exponent = ((self.r_cutoff / r) - 1.0) ** 2
+            return math.exp(-self.expon_coeff * exponent)
