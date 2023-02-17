9db0e4b5b3ad4f8d00dba4e67dfd24a2e58675a5
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-02-17 14:58:58 -0500

    Add checks to the parameters used in the functions

--- src/dispersion4b/shortrange/short_range_functions.py
+++ src/dispersion4b/shortrange/short_range_functions.py
@@ -3,13 +3,42 @@ This module contains functions used as the short-range potential portion of the
 'FourBodyAnalyticPotential'.
 """
 
-import numpy as np
+import math
+from dataclasses import dataclass
 
 
-def exponential_decay(r: float, a: float, b: float) -> float:
-    return a * np.exp(-b * r)
+@dataclass(frozen=True)
+class ExponentialDecay:
+    coeff: float
+    expon: float
+
+    def __post_init__(self) -> None:
+        if self.expon <= 0.0:
+            raise ValueError(
+                "The coefficient in the exponent for the exponential decay must be positive.\n"
+                f"Entered: {self.expon: .12f}"
+            )
+
+    def __call__(self, x: float) -> float:
+        return self.coeff * math.exp(-self.expon * x)
 
 
-def exponential_decay_order2(r: float, a: float, b0: float, b1: float) -> float:
-    exponent = (b0 * r) + (b1 * r**2)
-    return a * np.exp(-exponent)
+@dataclass(frozen=True)
+class ExponentialDecayOrder2:
+    coeff: float
+    expon_lin: float
+    expon_sq: float
+
+    def __post_init__(self) -> None:
+        # it is *technically* fine for convergence if the coefficient for the linear term in
+        # the exponent is nonpositive; as long as the coefficient for the squared term is
+        # positive, the function will eventually converge
+        if self.expon_sq <= 0.0:
+            raise ValueError(
+                "The coefficient in the exponent for the squared term must be positive.\n"
+                f"Entered: {self.expon_sq: .12f}"
+            )
+
+    def __call__(self, x: float) -> float:
+        exponent = (self.expon_lin * x) + (self.expon_sq * x**2)
+        return self.coeff * math.exp(-exponent)
