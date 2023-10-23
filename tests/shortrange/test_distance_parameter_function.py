ce94ec6252d79b6e777cdd35826614ab29a691e6
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-10-23 19:13:54 -0400

    Linting stuff

--- tests/shortrange/test_distance_parameter_function.py
+++ tests/shortrange/test_distance_parameter_function.py
@@ -8,25 +8,26 @@ from cartesian.measure import euclidean_distance as distance
 from dispersion4b.shortrange.distance_parameter_function import sum_of_sidelengths
 from dispersion4b.shortrange.distance_parameter_function import sum_of_com_distances
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def unit_tetrahedron_points():
     lat_const = 1.0
     points = [
         lat_const * Cartesian3D(-0.5, 0.0, 0.0),
-        lat_const * Cartesian3D( 0.5, 0.0, 0.0),
-        lat_const * Cartesian3D( 0.0, math.sqrt(3.0/4.0), 0.0),
-        lat_const * Cartesian3D( 0.0, math.sqrt(1.0/12.0), math.sqrt(2.0/3.0)),
+        lat_const * Cartesian3D(0.5, 0.0, 0.0),
+        lat_const * Cartesian3D(0.0, math.sqrt(3.0 / 4.0), 0.0),
+        lat_const * Cartesian3D(0.0, math.sqrt(1.0 / 12.0), math.sqrt(2.0 / 3.0)),
     ]
 
     yield points
-    
 
-@pytest.mark.usefixtures('unit_tetrahedron_points')
+
+@pytest.mark.usefixtures("unit_tetrahedron_points")
 def test_sum_of_sidelengths(unit_tetrahedron_points):
     assert sum_of_sidelengths(unit_tetrahedron_points) == pytest.approx(6.0)
 
 
-@pytest.mark.usefixtures('unit_tetrahedron_points')
+@pytest.mark.usefixtures("unit_tetrahedron_points")
 def test_sum_of_com_distances(unit_tetrahedron_points):
     com = centroid(unit_tetrahedron_points)
     dist = distance(com, unit_tetrahedron_points[0])
--- tests/shortrange/test_short_range_functions.py
+++ tests/shortrange/test_short_range_functions.py
@@ -6,9 +6,9 @@ from dataclasses import dataclass
 from dispersion4b.shortrange.short_range_functions import ExponentialDecay
 from dispersion4b.shortrange.short_range_functions import ExponentialDecayOrder2
 
-@pytest.fixture(scope='class')
-def ed_args():
 
+@pytest.fixture(scope="class")
+def ed_args():
     @dataclass(frozen=True)
     class ExponentialDecayArguments:
         coeff: float
@@ -17,23 +17,22 @@ def ed_args():
     return ExponentialDecayArguments(1.0, 1.0)
 
 
-@pytest.mark.usefixtures('ed_args')
+@pytest.mark.usefixtures("ed_args")
 class TestExponentialDecay:
     def test_basic_functionality(self, ed_args):
         ed = ExponentialDecay(ed_args.coeff, ed_args.expon)
-        
+
         assert ed(0.0) == pytest.approx(1.0)
         assert ed(3.0) < ed(2.0) < ed(1.0) < ed_args.coeff
 
-    @pytest.mark.parametrize('bad_expon', [-1.0, 0.0])
+    @pytest.mark.parametrize("bad_expon", [-1.0, 0.0])
     def test_raises_nonpositive_expon(self, bad_expon, ed_args):
         with pytest.raises(ValueError):
             ExponentialDecay(ed_args.coeff, bad_expon)
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope="class")
 def edo2_args():
-
     @dataclass(frozen=True)
     class ExponentialDecayOrder2Arguments:
         coeff: float
@@ -43,20 +42,25 @@ def edo2_args():
     return ExponentialDecayOrder2Arguments(1.0, 1.0, 1.0)
 
 
-@pytest.mark.usefixtures('edo2_args')
+@pytest.mark.usefixtures("edo2_args")
 class TestExponentialDecayOrder2:
     def test_basic_functionality(self, edo2_args):
-        edo2 = ExponentialDecayOrder2(edo2_args.coeff, edo2_args.expon_lin, edo2_args.expon_sq)
-        
+        edo2 = ExponentialDecayOrder2(
+            edo2_args.coeff, edo2_args.expon_lin, edo2_args.expon_sq
+        )
+
         assert edo2(0.0) == pytest.approx(1.0)
         assert edo2(3.0) < edo2(2.0) < edo2(1.0) < edo2_args.coeff
 
-    @pytest.mark.parametrize('bad_expon_sq', [-1.0, 0.0])
+    @pytest.mark.parametrize("bad_expon_sq", [-1.0, 0.0])
     def test_raises_nonpositive_expon_sq(self, bad_expon_sq, edo2_args):
         with pytest.raises(ValueError):
             ExponentialDecayOrder2(edo2_args.coeff, edo2_args.expon_lin, bad_expon_sq)
 
-    @pytest.mark.parametrize('expon_lin', [-1.0, 0.0, 1.0])
+    @pytest.mark.parametrize("expon_lin", [-1.0, 0.0, 1.0])
     def test_all_expon_lin_allowed(self, expon_lin, edo2_args):
         with nullcontext():
-            assert ExponentialDecayOrder2(edo2_args.coeff, expon_lin, edo2_args.expon_sq) is not None
+            assert (
+                ExponentialDecayOrder2(edo2_args.coeff, expon_lin, edo2_args.expon_sq)
+                is not None
+            )
