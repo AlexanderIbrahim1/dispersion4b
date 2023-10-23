9d1860a6972756b91d6d7f6fa3d8068ccf23a126
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-10-23 15:17:06 -0400

    Add tests for MagnitudeAndDirection for dispersion

---
+++ tests/test_magnitude_and_direction.py
@@ -0,0 +1,34 @@
+import math
+
+import pytest
+
+from cartesian import Cartesian3D
+from cartesian.measure import approx_eq
+from cartesian.measure import euclidean_norm
+
+from dispersion4b.potential import MagnitudeAndDirection
+from dispersion4b.potential import _distance_and_unit_vector
+
+
+def test_unit_vector():
+    p0 = Cartesian3D(0.0, 0.0, 0.0)
+    p1 = Cartesian3D(1.0, 0.0, 0.0)
+    mad = _distance_and_unit_vector(p0, p1)
+
+    expected_magnitude = 1.0
+
+    assert mad.magnitude == pytest.approx(expected_magnitude)
+    assert approx_eq(mad.direction, Cartesian3D(-1.0, 0.0, 0.0))
+    assert euclidean_norm(mad.direction) == pytest.approx(1.0)
+
+
+def test_corner_vector():
+    p0 = Cartesian3D(0.0, 0.0, 0.0)
+    p1 = Cartesian3D(1.0, 2.0, 3.0)
+    mad = _distance_and_unit_vector(p0, p1)
+
+    expected_magnitude = euclidean_norm(p1 - p0)
+
+    assert mad.magnitude == pytest.approx(expected_magnitude)
+    assert approx_eq(mad.direction, Cartesian3D(-1.0, -2.0, -3.0) / expected_magnitude)
+    assert euclidean_norm(mad.direction) == pytest.approx(1.0)
